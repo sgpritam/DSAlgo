30fe1e7b564c9904b14d82c3846d314682cfb91e
Pritam <pritam1bnk@gmail.com>
2020-03-10 01:03:50 +0530

    code modified

--- Bootcamp/Practice/smallestAndLargestInArray.cpp
+++ Bootcamp/Practice/smallestAndLargestInArray.cpp
@@ -15,12 +15,15 @@ int main(){
     int smallest=INT_MAX;
 
     for(int i=0;i<n;i++){
-        if(a[i]>largest){
-            largest=a[i];
-        }
-        if(a[i]<smallest){
-            smallest=a[i];
-        }
+        // if(a[i]>largest){
+        //     largest=a[i];
+        // }
+        // if(a[i]<smallest){
+        //     smallest=a[i];
+        // }
+
+        largest = max(largest,a[i]);
+        smallest=min(smallest,a[i]);
     }
     cout<<" largest : "<<largest<<endl;
     cout<<" smallest : "<<smallest<<endl;
