28e27719c5360cab4f9a22ea669e2bb3c6b6c33c
Pritam <pritam1bnk@gmail.com>
2020-03-18 00:00:50 +0530

    some code

---
+++ Bootcamp/Practice/LargestString.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<cstring>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    char a[1000];
+    char largest[1000];
+
+    int len=0;
+    int largest_len=0;
+
+    cin.get(); /// For ignoring null character
+    
+    for(int i=0;i<n;i++){
+        cin.getline(a,1000);
+        // cout<<a<<endl;
+        len = strlen(a);
+        if(len > largest_len){
+            largest_len = len;
+            strcpy(largest,a);
+        }
+    }
+    cout<<largest<<" and "<<largest_len<<endl;
+    return 0;
+}
